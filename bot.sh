01fafd93e8ee0d67b60753e9467a63368c50867a
micodev <micoadult2018@gmail.com>
2020-07-12 02:01:08 +0300

    init commit

--- bot.sh
+++ bot.sh
@@ -2,8 +2,8 @@ sudo apt install software-properties-common
 udo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install python3.6
-sudo apt install libcurl4-gnutls-dev
-pip3 install -r requirements.txt
+sudo apt-get install python3.6-dev
+python3.6 -m pip install -r requirements.txt
 echo -n "Insert api hash (https://my.telegram.org) : "
 read api_hash
 echo -n "Insert api id : "
@@ -15,4 +15,4 @@ read id
 file=config.json
 echo 
 echo '{\n"api_hash": "'$api_hash'",\n"api_id": '$api_id',"bot_id": 0,"isbot": true,"plugins": ["plugins","help"],"sudo_members": ['$id']}' > $file
-python3 main.py
+python3.6 main.py
