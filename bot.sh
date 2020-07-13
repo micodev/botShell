84ffdc35e6e0c367b035b16f6e917a2812532e79
micodev <micoadult2018@gmail.com>
2020-07-13 13:11:04 +0300

    update bash (support activate all plugins in first run)

--- bot.sh
+++ bot.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update -y
@@ -13,8 +14,15 @@ read api_id
 echo -n "Insert the id of main user (you id) : "
 read id
 
+search_dir=$(cd $(dirname $0); pwd)
+array=("item 1" "item 2" "item 3")
+for file in $search_dir/plugins/*; do       
+  f=$(echo "${file##*/}");
+  filename=$(echo $f| cut  -d'.' -f 1); #file has extension, it return only filename
+  plugins+="\""$filename"\","
+done
+plugins=${plugins[@]%?}
 
 file=config.json
-echo
-echo '{\n"api_hash": "'$api_hash'",\n"api_id": '$api_id',"bot_id": 0,"isbot": true,"plugins": ["plugins","help"],"sudo_members": ['$id']}' > $file
+echo '{"api_hash": "'$api_hash'","api_id": '$api_id',"bot_id": 0,"isbot": true,"plugins":['$plugins'],"sudo_members": ['$id']}' > $file
 python3.6 main.py
