c25aadd59784554fb581b002afcd6f3f771cd82d
micodev <ibrahimayad321@gmail.com>
2020-07-18 01:10:33 +0300

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash/
 pkill -f main.py
+mv config.json .config.json
+git reset --hard
+git pull
+mv .config.json config.json
 hn=$(hostname)
 python3 -m install -r requirements.txt
 python3 main.py
