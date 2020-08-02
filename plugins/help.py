60de0595729c100ceb8c5c50cdd3517767421c43
micodev <ibrahimayad321@gmail.com>
2020-08-02 15:20:39 +0300

    update help

--- plugins/help.py
+++ plugins/help.py
@@ -15,6 +15,7 @@ def getallUsage(name=None):
             for files in os.listdir(join(utilities.WD, "plugins"))
             if re.search("^(.*)\.py$", files)
         ]
+    msgs = []
     for plugin_file in plugin_files:
         plugin_file = plugin_file.replace(".py", "")
         if plugin_file == "__init__":
@@ -25,28 +26,37 @@ def getallUsage(name=None):
                 response_text += (
                     "ℹ️ "
                     + plugin["name"]
-                    + "'s usage :\n\n"
+                    + "'s usage :\n"
                     + "".join(((i + "\n")) for i in plugin["usage"])
                     + "\n"
                     + ("" if name == None else "Description : " + plugin["desc"])
                 )
             else:
-
                 response_text += (
                     "ℹ️ "
                     + plugin["name"]
-                    + "'s patterns :\n\n"
+                    + "'s patterns :\n"
                     + "".join((i + "\n") for i in plugin["patterns"])
                     + "\n"
                 )
-    return response_text if (response_text != "") else "no such a plugin"
+            if len(response_text) > 3500:
+                msgs.append(response_text)
+                response_text = ""
+    if len(response_text) > 0:
+        msgs.append(response_text)
+    return msgs if (len(msgs) > 0) else "no such a plugin"
 
 
 async def run(message, matches, chat_id, step, crons=None):
+    response = []
     if matches[1:] == "help":
-        return [message.reply(getallUsage(), parse_mode=None)]
+        for i in getallUsage():
+            response.append(message.reply(i, parse_mode=None))
+        return response
     else:
-        return [message.reply(getallUsage(matches), parse_mode=None)]
+        for i in getallUsage(matches):
+            response.append(message.reply(i, parse_mode=None))
+        return response
 
 
 plugin = {
