3233a803a5ccdcee6ba4cfced6b6ebcf200841ce
micodev <ibrahimayad321@gmail.com>
2020-08-04 20:16:01 +0300

    update

--- bot.sh
+++ bot.sh
@@ -22,5 +22,6 @@ then
 
   file=config.json
   echo '{"db":"sqlite:///database.db","api_hash": "'$api_hash'","api_id": '$api_id',"bot_id": 0,"isbot": true,"plugins":['$plugins'],"sudo_members": ['$id']}' > $file
+  py=python
   $py main.py
 fi
