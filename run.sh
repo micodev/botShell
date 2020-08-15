63ac36c48300a756747a4e6283076e9168edddd2
micodev <ibrahimayad321@gmail.com>
2020-08-15 15:45:13 +0300

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash/
 pkill -f main.py
-pkill -f run.sh
 mv config.json .config.json
 git reset --hard
 git pull
 mv .config.json config.json
-sudo update-alternatives  --set python /usr/bin/python3.6
+sudo update-alternatives  --set python /usr/bin/python3.6 0
 sudo update-alternatives --config python
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
