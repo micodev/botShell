3538ffffa450d09b341b6eaac3d7ba168a0e4fb4
micodev <ibrahimayad321@gmail.com>
2020-08-15 16:00:56 +0300

    update

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ mv config.json .config.json
 git reset --hard
 git pull
 mv .config.json config.json
-sudo update-alternatives  --set python /usr/bin/python3.6 0
-sudo update-alternatives --config python
+sudo update-alternatives  --set python /usr/bin/python3.6 
+echo -ne '\n' | sudo update-alternatives --config python 
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
 python -m pip install -r requirements.txt
