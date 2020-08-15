1f1e76e6b8847316b4ec1c8632b695716dda5dfa
micodev <ibrahimayad321@gmail.com>
2020-08-15 16:04:11 +0300

    update

--- lib.sh
+++ lib.sh
@@ -13,6 +13,7 @@ then
     pkg install libjpeg-turbo
     pkg install ffmpeg
     pkg install qpdf
+    pkg install zlib
     redis-server --daemonize yes
     $pi install -r requirements.txt
 
@@ -24,6 +25,7 @@ else
     sudo apt install python3-pip -y
     sudo apt install ffmpeg -y
     sudo apt install redis-server -y
+    sudo apt install clang lib{jpeg-turbo,webp}-dev python3.6{,-dev} zlib-dev
     sudo service redis-server restart
     sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 10
     py=python
