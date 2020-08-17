cdfce834b3b8db54b7a9e3df2d10896e61939884
micodev <ibrahimayad321@gmail.com>
2020-08-17 17:24:03 +0300

    update

--- lib.sh
+++ lib.sh
@@ -19,17 +19,17 @@ then
     $pi install -r requirements.txt
 
 else
-    sudo apt install software-properties-common -y
-    sudo add-apt-repository ppa:deadsnakes/ppa -y
     sudo apt update -y
-    sudo apt install python3.6 -y
+    sudo apt install -y software-properties-common
+    echo -ne '\n' | sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt install python3.8 -y
     sudo apt install python3-pip -y
+    python3.8 -m pip install --upgrade pip
     sudo apt install ffmpeg -y
     sudo apt install redis-server -y
     sudo apt install clang -y
     sudo service redis-server restart
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 10
-    py=python
+    py=python3.8
     pi=$py" -m pip"
     $pi install --upgrade pip
     $pi install setuptools
