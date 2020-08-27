6006a72bd5dd0951ade506d6eb43b6b16013fd96
micodev <ibrahimayad321@gmail.com>
2020-08-27 20:32:22 +0300

    update

--- plugins/autoReply.py
+++ plugins/autoReply.py
@@ -9,62 +9,66 @@ propName = "autoReply"
 
 async def run(message, matches, chat_id, step, crons=None):
     response = []
-
-    if matches == "ea":
-        isActive = getPropSetting(message.chat_id, propName) or None
-        if isActive and isActive.active:
-            response.append(message.reply("auto reply already activated."))
-        else:
-            if togglePropSettings(message.chat_id, propName):
-                response.append(message.reply("auto reply has been activated."))
+    if(utilities.check_sudo(message.sender_id)):
+        if matches == "ea":
+            isActive = getPropSetting(message.chat_id, propName) or None
+            if isActive and isActive.active:
+                response.append(message.reply("auto reply already activated."))
             else:
-                response.append(message.reply("error while doing that."))
-    elif matches == "da":
-        isActive = getPropSetting(message.chat_id, propName) or None
-        if not isActive or isActive.active:
-            if togglePropSettings(message.chat_id, propName, False):
-                response.append(message.reply("auto reply has been deactivated."))
-            else:
-                response.append(message.reply("error while doing that."))
-        else:
-            response.append(message.reply("auto reply already deactivated."))
-    elif matches[0] == "ar":
-        bot = utilities.config["isbot"]
-        if message.is_reply:
-            msg = await message.get_reply_message()
-            if msg.media:
-                if bot:
-                    file = utils.pack_bot_file_id(msg.media)
-                    await utilities.client.send_file(msg.chat_id, file)
+                if togglePropSettings(message.chat_id, propName):
+                    response.append(message.reply("auto reply has been activated."))
                 else:
-                    downlodd_file = await msg.download_media("tmp/autoReply")
-                    up_file = await utilities.client.upload_file(
-                        downlodd_file, use_cache=True
-                    )
-                    sent_file = await utilities.client.send_file(msg.chat_id, up_file)
-                    file = utils.pack_bot_file_id(sent_file.media)
-                addAutoReply(matches[1], "media", msg.text, file)
-            else:
-                addAutoReply(matches[1], "text", msg.text)
-            response.append(message.reply("reply has been added"))
-        else:
-            response.append(message.reply("please reply to a message."))
-    elif matches[0] == "rar":
-        if remAutoReplySetting(matches[1]):
-            response.append(message.reply("reply has been deleted"))
-        else:
-            response.append(message.reply("no reply has that text"))
-    else:
-        isActive = getPropSetting(message.chat_id, propName) or None
-        if isActive and isActive.active:
-            rep = getAutoReply(message.text)
-            if rep:
-                if rep.msg_type == "media":
-                    return [message.reply(file=rep.file_id, message=rep.msg_content)]
+                    response.append(message.reply("error while doing that."))
+            return response
+        elif matches == "da":
+            isActive = getPropSetting(message.chat_id, propName) or None
+            if not isActive or isActive.active:
+                if togglePropSettings(message.chat_id, propName, False):
+                    response.append(message.reply("auto reply has been deactivated."))
                 else:
-                    return [message.reply(rep.msg_content)]
+                    response.append(message.reply("error while doing that."))
             else:
-                return response
+                response.append(message.reply("auto reply already deactivated."))
+            return response
+        elif matches[0] == "ar":
+            bot = utilities.config["isbot"]
+            if message.is_reply:
+                msg = await message.get_reply_message()
+                if msg.media:
+                    if bot:
+                        file = utils.pack_bot_file_id(msg.media)
+                        await utilities.client.send_file(msg.chat_id, file)
+                    else:
+                        downlodd_file = await msg.download_media("tmp/autoReply")
+                        up_file = await utilities.client.upload_file(
+                            downlodd_file, use_cache=True
+                        )
+                        sent_file = await utilities.client.send_file(msg.chat_id, up_file)
+                        file = utils.pack_bot_file_id(sent_file.media)
+                    addAutoReply(matches[1], "media", msg.text, file)
+                else:
+                    addAutoReply(matches[1], "text", msg.text)
+                response.append(message.reply("reply has been added"))
+            else:
+                response.append(message.reply("please reply to a message."))
+            return response
+        elif matches[0] == "rar":
+            if remAutoReplySetting(matches[1]):
+                response.append(message.reply("reply has been deleted"))
+            else:
+                response.append(message.reply("no reply has that text"))
+            return response
+    
+    isActive = getPropSetting(message.chat_id, propName) or None
+    if isActive and isActive.active:
+        rep = getAutoReply(message.text)
+        if rep:
+            if rep.msg_type == "media":
+                return [message.reply(file=rep.file_id, message=rep.msg_content)]
+            else:
+                return [message.reply(rep.msg_content)]
+        else:
+            return response
     return response
 
 
@@ -78,7 +82,7 @@ plugin = {
         "/ar <text> reply to a message to save it.",
         "/rar <query> to delete reply.",
     ],
-    "sudo": True,
+    "sudo": False,
     "patterns": [
         "^[!/#](ea)$",
         "^[!/#](da)$",
